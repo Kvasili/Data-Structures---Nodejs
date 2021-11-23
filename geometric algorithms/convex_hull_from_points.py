db9479528df075b8493b20ca373f27c2d5361617
Kvasili <k.vasili913@gmail.com>
2021-11-24 01:41:39 +0200

    update

---
+++ geometric algorithms/convex_hull_from_points.py
@@ -0,0 +1,21 @@
+'''
+    @Author: Konstantinos V
+    @Date: November 2021
+
+    Description: Given a set of points, find the smallest convex polygon containing all the points
+
+    input: a set of 2d points
+    output: the convex hull polygon enclosing those points
+
+
+    python convex_hull_from_points.py
+
+'''
+import time
+
+start = time.time()
+
+
+end = time.time()
+
+print("{}".format(end-start))
