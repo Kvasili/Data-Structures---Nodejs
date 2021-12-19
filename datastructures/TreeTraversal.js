23c59cb480d28d7ef88160d286490a42b1851021
Kvasili <k.vasili913@gmail.com>
2021-12-19 14:14:02 +0200

    insert Tree Traversal

---
+++ datastructures/TreeTraversal.js
@@ -0,0 +1,13 @@
+/**
+ * @Author Konstantinos Vasili
+ * @Date December 2021
+ * 
+ * Implementation of a Tree Traversal using the Breadth First Search (BFS)
+ * approach. Every node is visited horizontally
+ * 
+ * 
+ * USAGE
+ * node TreeTraversal.js
+ * 
+ */
+
